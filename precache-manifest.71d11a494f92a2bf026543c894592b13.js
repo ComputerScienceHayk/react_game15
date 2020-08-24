b3127e9f760363080970cd0c7c9181d3503d8b0a
ComputerScienceHayk <haikazbashoyan@gmail.com>
2020-08-24 20:29:10 +0400

    Updates

---
+++ precache-manifest.71d11a494f92a2bf026543c894592b13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41d34804743f9a17dbe385b96b339e76",
+    "url": "/react_game15/index.html"
+  },
+  {
+    "revision": "9879667431ba13f914da",
+    "url": "/react_game15/static/css/main.5029d4bf.chunk.css"
+  },
+  {
+    "revision": "49d4df89876238af1319",
+    "url": "/react_game15/static/js/2.3f2270b1.chunk.js"
+  },
+  {
+    "revision": "5d76ba339be737b85e0cb30d4b8e7ba5",
+    "url": "/react_game15/static/js/2.3f2270b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9879667431ba13f914da",
+    "url": "/react_game15/static/js/main.644e6704.chunk.js"
+  },
+  {
+    "revision": "156be62ba8f5456a7a02",
+    "url": "/react_game15/static/js/runtime-main.a8d167a5.js"
+  },
+  {
+    "revision": "16d26a3b5b24871cb9dc660bf59d935d",
+    "url": "/react_game15/static/media/background.16d26a3b.jpg"
+  }
+]);
